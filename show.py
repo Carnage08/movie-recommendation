a90e9031b16e9290058486cf4c045c008f5fc649
Arjun Yadav <68326824+Carnage08@users.noreply.github.com>
2025-01-14 19:00:14 +0530

    Add files via upload

---
+++ show.py
@@ -0,0 +1,25 @@
+#distances = [0.9, 0.8, 0.5, 0.7]
+#(index, similarity_score)
+enumerate(distances) = [(0, 0.9), (1, 0.8), (2, 0.5), (3, 0.7)]
+
+
+
+
+
+
+#similarity = [
+#  [1.0, 0.8, 0.3],
+#  [0.8, 1.0, 0.4],
+#  [0.3, 0.4, 1.0]
+#]
+
+#similarity[0]
+
+#[1.0, 0.8, 0.3]
+
+
+#movies.iloc[1] = 
+#    title      Titanic
+#    movie_id   597
+#    overview   ...
+#    genres     ...
